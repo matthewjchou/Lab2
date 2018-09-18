d337445f4b77f1dcd7813dd19185a22864a8dba3
Matthew Chou <matthew.chouj@gmail.com>
2018-09-18 16:49:46 -0500

    Attempted something?

--- Lab_3/src/WebScraper.java
+++ Lab_3/src/WebScraper.java
@@ -1,5 +1,50 @@
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
+
+
 public class WebScraper {
+
+
+    /**
+     * Retrieve contents from a URL and return them as a string.
+     *
+     * @param url url to retrieve contents from
+     * @return the contents from the url as a string, or an empty string on error
+     */
+    public static String urlToString(final String url) {
+        Scanner urlScanner;
+        try {
+            urlScanner = new Scanner(new URL(url).openStream(), "UTF-8");
+        } catch (IOException e) {
+            return "";
+        }
+        String contents = urlScanner.useDelimiter("\\A").next();
+        urlScanner.close();
+        return contents;
+    }
+
+    /**
+     * \
+     * @param unused idk either man
+     */
+
     public static void main(String[] unused) {
-        System.out.println("here");
+        String url1 = "http://erdani.com/tdpl/hamlet.txt";
+        String url2 = "https://www.bls.gov/tus/charts/chart9.txt";
+        String url3 = "http://tgftp.nws.noaa.gov/data/raw/fz/fzus53.klot.srf.lot.txt";
+
+        String trimmed = urlToString(url1).trim();
+        String[] splitString = trimmed.split(" ");
+        System.out.println(splitString.length);
+
+        trimmed = urlToString(url2).trim();
+        splitString = trimmed.split(" ");
+        System.out.println(splitString.length);
+
+        trimmed = urlToString(url3).trim();
+        splitString = trimmed.split(" ");
+        System.out.println(splitString.length);
+
     }
 }
